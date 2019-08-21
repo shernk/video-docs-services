35a1531b8158c979844d5167313adf108691ba34
shernk <31926953+shernk@users.noreply.github.com>
2019-08-21 09:40:41 +0700

    Added dotenv and body-parser

--- src/app.model.ts
+++ src/app.model.ts
@@ -1,5 +1,5 @@
-import bodyParser = require("body-parser");
-import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import express from "express";
 import mongoose from "mongoose";
 
 class App {
@@ -13,9 +13,8 @@ class App {
   }
 
   private config(): void {
-    // this.app.use(bodyParser.json());
-    // this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(express.json());
+    dotenv.config();
   }
 
   private mongoSetup() {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import app from "./app.model";
 import V1Routes from "./routes/v1/v1.route";
 
+// version routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://www.videodevdocs.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // defines version routes
 app.use("/api/v1", V1Routes);

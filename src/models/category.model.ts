b049959eff93e33fcee911ac9605abb1250c3c0d
shernk <31926953+shernk@users.noreply.github.com>
2019-09-19 23:05:26 +0700

    show playlist's Item-Ep29-29:21

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -4,7 +4,15 @@ import { ICategory } from "./category/category.interface";
 const CategorySchema = new Schema({
   simpleId: { type: Object, required: true, max: 20, min: 2 },
   label: { type: String, requried: true, max: 50, min: 2, default: "" },
+
+  /*
+   TODO: watch again
+   * Ep29
+   * Time: 29:21
+   ! show playlist's item[{videoID, desciption, title, thumnailUrl}]
+   */
   playlist: { type: Object, required: false },
+
   playlistId: { type: String, required: true },
   description: { type: String, maxlength: 200, default: "" },
   books: { type: Array, required: false, default: [] },

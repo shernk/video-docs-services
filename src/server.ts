889a6b8bfc8991fdf94c66e4d7c8bb94c291d8d7
shernk <31926953+shernk@users.noreply.github.com>
2019-09-20 00:45:08 +0700

    Modified ACAO: https://www.videodevdocs.com

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import V1Routes from "./routes/v1/v1.route";
 
 // version routes
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://www.videodevdocs.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
